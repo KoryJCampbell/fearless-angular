7fc3a90c585750fc34acddb37c107ea3d2ddb467
Kory Campbell <korycampbell@Korys-MacBook-Pro.local>
2021-03-21 12:58:56 -0400

    init comment w/ countapi import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as countapi from 'countapi-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fearless-angular';
+
+
+export class AppComponent implements OnInit {
+
+  value:any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+    countapi.visits().then((result) => {
+
+      this.value = result.value
+      console.log(this.value);
+    });
+  }
+
+
+
+
 }
